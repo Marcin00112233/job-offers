efcc8bcecad608c83578d403cdf34f9ed838e765
Marcin Bartliński <max.29@o2.pl>
2018-04-08 11:57:46 +0200

    configuration data

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/*olx */
+$address = "http://olx.pl/praca/informatyka/mazowieckie/?search%5Bfilter_enum_contract%5D%5B0%5D=contract"; //adres strony z ofertami olx
+$initial_sequence = '<a class="link"';               //ciąg początkowy
+$final_sequence = "</a>"; 
+$initial_sequence_offer_olx = '<div class="clr" id="textContent">';
+$final_sequence_offer_olx = "</div>"                          //ciąg końcowy
+
+?>
